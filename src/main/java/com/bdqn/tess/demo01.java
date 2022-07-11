89fede80cd879454848982e2bd6831f81f1b948e
zhao <2823178077@qq.com>
2022-07-11 17:08:08 +0800

    two commit

---
+++ src/main/java/com/bdqn/tess/demo01.java
@@ -0,0 +1,8 @@
+package com.bdqn.tess;
+
+public class demo01 {
+    public static void main(String[] args) {
+        System.out.println("eeeeeeee");
+        System.out.println("eeeeeeee");
+    }
+}
